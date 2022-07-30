f1624d8a98be739d8bf837769d3c1188eae19869
Boeun <boeun0904@naver.com>
2022-07-30 17:12:20 +0900

    Feat: 사진 접근 권한 요청, 사진 불러오기

--- app/src/main/java/com/han/collector/view/fragments/AddPlaceFragment.kt
+++ app/src/main/java/com/han/collector/view/fragments/AddPlaceFragment.kt
@@ -1,14 +1,27 @@
 package com.han.collector.view.fragments
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.han.collector.databinding.FragmentAddPlaceBinding
+import com.han.collector.utils.Constants
 import com.han.collector.viewmodel.PlaceViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.FlowPreview
+import java.net.URI
 import kotlin.experimental.and
 
 @SuppressLint("ClickableViewAccessibility")
@@ -27,6 +40,7 @@ class AddPlaceFragment : Fragment() {
         binding = FragmentAddPlaceBinding.inflate(inflater, container, false)
 
         binding?.viewModel = viewModel
+        binding?.fragment = this
         binding?.lifecycleOwner = this
 
         binding?.etGoods?.setOnTouchListener(onTouchListener)
@@ -34,6 +48,82 @@ class AddPlaceFragment : Fragment() {
         binding?.etMemo?.setOnTouchListener(onTouchListener)
 
         return binding?.root
+    }
+
+    val getGalleryImageLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            with(result) {
+                when (resultCode) {
+                    Activity.RESULT_OK -> {
+                        val uri = data?.data as Uri
+                        binding?.ivImage?.scaleType = ImageView.ScaleType.CENTER_CROP
+                        binding?.ivImage?.setImageURI(uri)
+                    }
+                    Activity.RESULT_CANCELED -> {
+                        Toast.makeText(
+                            context, "사진을 가져오지 못했습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+
+        }
+
+    fun startGalleryApp(){
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        getGalleryImageLauncher.launch(intent)
+    }
+
+    val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                startGalleryApp()
+            } else {
+                Toast.makeText(
+                    context, "사진 접근 권한이 거부되었습니다.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    fun requestPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                startGalleryApp()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                MaterialAlertDialogBuilder(requireContext())
+                    .setTitle("권한 요청")
+                    .setMessage("이 기능을 이용하려면 사진 접근 권한을 허용해야 합니다.")
+                    .setNegativeButton("취소") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .setPositiveButton("확인") { dialog, _ ->
+                        dialog.dismiss()
+                        requestPermissionLauncher.launch(
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                    }
+                    .show()
+            }
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            }
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 
     val onTouchListener = View.OnTouchListener { view, event ->
@@ -49,10 +139,5 @@ class AddPlaceFragment : Fragment() {
             }
         }
         false
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        binding = null
     }
 }
